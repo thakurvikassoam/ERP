f17e22feec0bf67064b2c9ddb93a8c6f41fa2d64
Vikas Soam <vikassoam9442@gmail.com>
2024-12-26 22:02:24 +0530

    edsfc

--- ERP/Program.cs
+++ ERP/Program.cs
@@ -1,4 +1,3 @@
-
 namespace ERP;
 
 public class Program
@@ -6,18 +5,13 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-        //builder.AddServiceDefaults();
 
         // Add services to the container.
-
         builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
 
         var app = builder.Build();
-
-       // app.MapDefaultEndpoints();
 
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
@@ -27,10 +21,7 @@ public class Program
         }
 
         app.UseHttpsRedirection();
-
         app.UseAuthorization();
-
-
         app.MapControllers();
 
         app.Run();
