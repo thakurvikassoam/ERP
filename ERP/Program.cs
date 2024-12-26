03f69ca1b7d82fd866a3c653407fe18c6eed945e
Vikas Soam <vikassoam9442@gmail.com>
2024-12-26 23:11:35 +0530

    fgcv b

--- ERP/Program.cs
+++ ERP/Program.cs
@@ -14,7 +14,7 @@ public class Program
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
+        if (!app.Environment.IsDevelopment())
         {
             app.UseSwagger();
             app.UseSwaggerUI();
