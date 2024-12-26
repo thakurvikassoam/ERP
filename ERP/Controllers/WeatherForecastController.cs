e539deebae06e8b93aac6c73dbe37981e39cb6a9
Vikas Soam <vikassoam9442@gmail.com>
2024-12-26 23:19:00 +0530

    fgv b

--- ERP/Controllers/WeatherForecastController.cs
+++ ERP/Controllers/WeatherForecastController.cs
@@ -29,4 +29,15 @@ public class WeatherForecastController : ControllerBase
         })
         .ToArray();
     }
+    [HttpGet(Name = "GetWeatherForecastdata")]
+    public IEnumerable<WeatherForecast> Getdata()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
 }
